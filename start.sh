3f23a0c09f0540220c10e1cc51bf74f276247a5b
j341nono <kanakana.nono1224@gmail.com>
2025-08-21 21:50:59 +0900

    fix: Add execute permission to script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 mkdir -p model
 cd model
 
-wget https://huggingface.co/unsloth/gemma-3-4b-it-GGUF/resolve/main/gemma-3-4b-it.Q4_K_M.gguf
+wget https://huggingface.co/unsloth/gemma-3-4b-it-GGUF/resolve/main/gemma-3-4b-it-Q4_0.gguf
